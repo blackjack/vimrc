a0d0f9d7bb2fb835047f8ba0737ea9776b5c8f64
Oleksandr Senkovych <bjsenya@gmail.com>
2015-10-19 12:04:46 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script=`realpath $0`
+dir=`dirname $script`
+
+ln -s ~/.vimrc $dir/vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +BundleInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer --system-libclang --gocode-completer
