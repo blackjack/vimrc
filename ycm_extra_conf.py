d1141d9137c3e29417e63ca4ca300697031536f9
Oleksandr Senkovych <osenkovych@flowtraders.com>
2018-02-02 16:27:43 +0100

    Reformat ycm_extra_conf, better git root detection

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -90,18 +90,28 @@ flags = [
     '/usr/include/kdevplatform'
 ]
 
+SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
 
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
 
-def IsHeaderFile( filename ):
-    extension = os.path.splitext( filename )[ 1 ]
-    return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in ['.h', '.hxx', '.hpp', '.hh']
+
+
+def get_git_root(wd):
+    root = subprocess.check_output(['git', 'rev-parse', '--git-dir'],
+                                   cwd=wd).strip()
+
+    while True:
+        basename = os.path.basename(root)
+        root = os.path.dirname(root)
+        if basename == ".git":
+            return root
 
 
 def get_compilation_database(filename):
     try:
-        root = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'],
-                                       cwd=os.path.dirname(filename)).strip()
+        root = get_git_root(os.path.dirname(filename))
         compile_commands = os.path.join(root, "build/compile_commands.json")
 
         if IsHeaderFile(filename):
@@ -118,61 +128,61 @@ def get_compilation_database(filename):
 
 
 def DirectoryOfThisScript():
-  return os.path.dirname(os.path.abspath(__file__))
+    return os.path.dirname(os.path.abspath(__file__))
 
 
 def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
-  if not working_directory:
-    return list(flags)
-  new_flags = []
-  make_next_absolute = False
-  path_flags = ['-isystem', '-I', '-iquote', '--sysroot=']
-  for flag in flags:
-    new_flag = flag
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = ['-isystem', '-I', '-iquote', '--sysroot=']
+    for flag in flags:
+        new_flag = flag
 
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith('/'):
-        new_flag = os.path.join(working_directory, flag)
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
 
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
 
-      if flag.startswith(path_flag):
-        path = flag[len(path_flag):]
-        new_flag = path_flag + os.path.join(working_directory, path)
-        break
+            if flag.startswith(path_flag):
+                path = flag[len(path_flag):]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
 
-    if new_flag:
-      new_flags.append(new_flag)
-  return new_flags
+        if new_flag:
+            new_flags.append(new_flag)
+    return new_flags
 
 
 def FlagsForFile(filename):
-  database = get_compilation_database(filename)
-  if database:
-    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object
-    compilation_info = database.GetCompilationInfoForFile(filename)
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-        compilation_info.compiler_flags_,
-        compilation_info.compiler_working_dir_)
+    database = get_compilation_database(filename)
+    if database:
+        # Bear in mind that compilation_info.compiler_flags_ does NOT return a
+        # python list, but a "list-like" StringVec object
+        compilation_info = database.GetCompilationInfoForFile(filename)
+        final_flags = MakeRelativePathsInFlagsAbsolute(
+            compilation_info.compiler_flags_,
+            compilation_info.compiler_working_dir_)
 
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% YOU NEED IT.
-    try:
-      final_flags.remove('-stdlib=libc++')
-    except ValueError:
-      pass
-    # raise Exception(str(final_flags))
-  else:
-    relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
+        # NOTE: This is just for YouCompleteMe; it's highly likely that your project
+        # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
+        # ycm_extra_conf IF YOU'RE NOT 100% YOU NEED IT.
+        try:
+            final_flags.remove('-stdlib=libc++')
+        except ValueError:
+            pass
+        # raise Exception(str(final_flags))
+    else:
+        relative_to = DirectoryOfThisScript()
+        final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
 
-  return {
-      'flags': final_flags,
-      'do_cache': True
-  }
+    return {
+        'flags': final_flags,
+        'do_cache': True
+    }
